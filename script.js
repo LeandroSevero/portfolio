da801bc173a3fd30e3cd7d39f1bf95cb4e449f4b
LeandroSevero <leandroolise@gmail.com>
2023-05-20 01:21:17 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,17 +8,11 @@ window.onload = function() {
     var novoLeft = windowWidth - imagem.offsetWidth;
     var novoTop = headerHeight + Math.floor(Math.random() * windowHeight);
     
-    imagem.style.transition = "none"; // Remover a transição temporariamente
-    imagem.style.opacity = 1; // Definir a opacidade como 1
-    
-    setTimeout(function() {
-      imagem.style.transform = "translate(" + novoLeft + "px, " + novoTop + "px)";
-      imagem.style.transition = "transform 5s ease-in-out"; // Restaurar a transição
-      imagem.style.opacity = 0.5; // Definir a opacidade desejada
-    }, 100); // Aguardar 100 milissegundos antes de iniciar a transição
+    imagem.style.transform = "translate(" + novoLeft + "px, " + novoTop + "px)";
   }
   
-  moverImagem(); // Mover a imagem imediatamente ao carregar o site
+  // Mover a imagem imediatamente ao carregar o site
+  moverImagem();
   
   setInterval(moverImagem, 5000); // Move a imagem a cada 5 segundos (5000 milissegundos)
 };

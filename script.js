60782b191d8e84859b7e6766d10fccf044a5d387
LeandroSevero <leandroolise@gmail.com>
2023-05-20 00:47:25 -0300

    20

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 window.onload = function() {
-  var imagem = document.getElementById("imagem-flutuando-js");
-  var containerWidth = document.getElementById("container").offsetWidth;
-  var containerHeight = document.getElementById("container").offsetHeight;
+  var imagem = document.getElementById("imagem");
+  var windowWidth = window.innerWidth;
+  var windowHeight = window.innerHeight;
   
   function moverImagem() {
-    var novoLeft = Math.floor(Math.random() * (containerWidth - imagem.width));
-    var novoTop = Math.floor(Math.random() * (containerHeight - imagem.height));
+    var novoLeft = Math.floor(Math.random() * windowWidth);
+    var novoTop = Math.floor(Math.random() * windowHeight);
     
     imagem.style.transform = "translate(" + novoLeft + "px, " + novoTop + "px)";
   }
   
-  setInterval(moverImagem, 3000); // Move a imagem a cada 3 segundos (3000 milissegundos)
+  setInterval(moverImagem, 5000); // Move a imagem a cada 5 segundos (5000 milissegundos)
 };

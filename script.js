f668d2821a5cda6c84be511a0523f1cb30916818
LeandroSevero <leandroolise@gmail.com>
2023-05-20 02:25:19 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,56 +17,40 @@ window.onload = function() {
   setInterval(moverImagem, 5000); // Move a imagem a cada 5 segundos (5000 milissegundos)
 
   var videos = document.querySelectorAll("#video-container video");
+  var photos = document.querySelectorAll("#photo-container img");
   var currentVideoIndex = 0;
+  var currentPhotoIndex = 0;
 
   function playNextVideo() {
     var currentVideo = videos[currentVideoIndex];
     var nextVideoIndex = (currentVideoIndex + 1) % videos.length;
     var nextVideo = videos[nextVideoIndex];
-  
-    currentVideo.style.opacity = 0;
+
+    currentVideo.style.display = "none";
     currentVideo.pause();
     currentVideo.currentTime = 0;
-  
-    nextVideo.style.opacity = 1;
+
+    nextVideo.style.display = "block";
     nextVideo.play();
-  
+
     currentVideoIndex = nextVideoIndex;
-  
-    // Transição suave entre vídeos
-    var fadeInterval = setInterval(function() {
-      if (nextVideo.style.opacity >= 1) {
-        clearInterval(fadeInterval);
-        return;
-      }
-  
-      currentVideo.style.opacity -= 0.05;
-      nextVideo.style.opacity = parseFloat(nextVideo.style.opacity) + 0.05;
-    }, 100);
   }
-  
-  // Exibir o primeiro vídeo imediatamente ao carregar o site
-  videos[currentVideoIndex].addEventListener("loadeddata", function() {
-    videos[currentVideoIndex].style.opacity = 1;
-    videos[currentVideoIndex].play();
-  });
-  
-  var photos = document.querySelectorAll("#photo-container img");
-  var currentPhotoIndex = 0;
 
   function showNextPhoto() {
     var currentPhoto = photos[currentPhotoIndex];
     var nextPhotoIndex = (currentPhotoIndex + 1) % photos.length;
     var nextPhoto = photos[nextPhotoIndex];
 
-    currentPhoto.style.opacity = 0;
-    nextPhoto.style.opacity = 1;
+    currentPhoto.style.display = "none";
+    nextPhoto.style.display = "block";
 
     currentPhotoIndex = nextPhotoIndex;
   }
 
-  // Exibir a primeira foto imediatamente ao carregar o site
+  // Exibir a primeira foto e reproduzir o primeiro vídeo imediatamente ao carregar o site
   showNextPhoto();
+  playNextVideo();
 
   setInterval(showNextPhoto, 5000); // Troca de foto a cada 5 segundos (5000 milissegundos)
+  setInterval(playNextVideo, 10000); // Reproduzir próximo vídeo a cada 10 segundos (10000 milissegundos)
 };

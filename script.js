da1f5ef0203ac667253f225ab562fa5baee19a1e
LeandroSevero <leandroolise@gmail.com>
2023-05-20 01:40:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,20 +3,19 @@ window.onload = function() {
   var headerHeight = document.getElementById("header").offsetHeight;
   var windowWidth = window.innerWidth;
   var windowHeight = window.innerHeight - headerHeight;
-  
+
   function moverImagem() {
     var novoLeft = windowWidth - imagem.offsetWidth;
     var novoTop = headerHeight + Math.floor(Math.random() * windowHeight);
-    
+
     imagem.style.transform = "translate(" + novoLeft + "px, " + novoTop + "px)";
   }
-  
+
   // Mover a imagem imediatamente ao carregar o site
   moverImagem();
-  
+
   setInterval(moverImagem, 5000); // Move a imagem a cada 5 segundos (5000 milissegundos)
-};
-window.onload = function() {
+
   var videos = document.querySelectorAll("#video-container video");
   var currentVideoIndex = 0;
 
@@ -32,8 +31,7 @@ window.onload = function() {
   }
 
   setInterval(playNextVideo, 5000); // Troca de vídeo a cada 5 segundos (5000 milissegundos)
-};
-window.onload = function() {
+
   var photos = document.querySelectorAll("#photo-container img");
   var currentPhotoIndex = 0;
 

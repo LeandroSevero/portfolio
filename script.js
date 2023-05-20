3440bbfabfdf9a18b196a9dc7a599de6d043608e
LeandroSevero <leandroolise@gmail.com>
2023-05-20 02:27:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -17,40 +17,46 @@ window.onload = function() {
   setInterval(moverImagem, 5000); // Move a imagem a cada 5 segundos (5000 milissegundos)
 
   var videos = document.querySelectorAll("#video-container video");
-  var photos = document.querySelectorAll("#photo-container img");
   var currentVideoIndex = 0;
-  var currentPhotoIndex = 0;
 
   function playNextVideo() {
     var currentVideo = videos[currentVideoIndex];
     var nextVideoIndex = (currentVideoIndex + 1) % videos.length;
     var nextVideo = videos[nextVideoIndex];
 
-    currentVideo.style.display = "none";
+    currentVideo.style.opacity = 0;
     currentVideo.pause();
-    currentVideo.currentTime = 0;
 
-    nextVideo.style.display = "block";
+    nextVideo.style.opacity = 1;
     nextVideo.play();
 
     currentVideoIndex = nextVideoIndex;
   }
+
+  // Exibir o primeiro vídeo imediatamente ao carregar o site
+  videos[currentVideoIndex].addEventListener("loadeddata", function() {
+    videos[currentVideoIndex].style.opacity = 1;
+    videos[currentVideoIndex].play();
+  });
+
+  setInterval(playNextVideo, 5000); // Troca de vídeo a cada 5 segundos (5000 milissegundos)
+
+  var photos = document.querySelectorAll("#photo-container img");
+  var currentPhotoIndex = 0;
 
   function showNextPhoto() {
     var currentPhoto = photos[currentPhotoIndex];
     var nextPhotoIndex = (currentPhotoIndex + 1) % photos.length;
     var nextPhoto = photos[nextPhotoIndex];
 
-    currentPhoto.style.display = "none";
-    nextPhoto.style.display = "block";
+    currentPhoto.style.opacity = 0;
+    nextPhoto.style.opacity = 1;
 
     currentPhotoIndex = nextPhotoIndex;
   }
-
-  // Exibir a primeira foto e reproduzir o primeiro vídeo imediatamente ao carregar o site
+  
+  // Exibir a primeira foto imediatamente ao carregar o site
   showNextPhoto();
-  playNextVideo();
 
   setInterval(showNextPhoto, 5000); // Troca de foto a cada 5 segundos (5000 milissegundos)
-  setInterval(playNextVideo, 10000); // Reproduzir próximo vídeo a cada 10 segundos (10000 milissegundos)
 };
